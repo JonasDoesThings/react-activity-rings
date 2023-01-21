af587fb9cbdca280e6ac59631ea4d52dbc5d8072
JonasDoesThings <jonas@jonasdoesthings.com>
2023-01-21 17:56:28 +0100

    build: remove dist folder from git

--- dist/dist/components/ActivityRings.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import type { ActivityRing, ActivityRingContainerOptions } from '../types';
-interface ActivityRingsProps {
-    rings: ActivityRing[];
-    options?: ActivityRingContainerOptions;
-}
-export default function ActivityRings(props: ActivityRingsProps): JSX.Element;
-export {};
--- dist/dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as ActivityRings } from './components/ActivityRings';
