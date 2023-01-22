e0eeb394f2699fff64783fbbbed741ca89e112a7
jonasdoesthings <jonas@jonasdoesthings.com>
2023-01-22 17:04:26 +0100

    docs: add doc-comments to interface fields

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,19 @@
 export interface ActivityRingContainerOptions {
+    // value of the css height property of the rings-container, defaults to 100%
     containerHeight?: string;
+    // value of the css width property of the rings-container, defaults to 100%
     containerWidth?: string;
+    // the padding between the single rings
     paddingBetweenRings?: number;
+    // the radius of the innermost ring
     initialRadius?: number;
+    // the duration of the appearance animation in milliseconds
     animationDurationMillis?: number;
 }
 
 export interface ActivityRing {
     // a float between 0-1 representing the filled-ness in percent
     filledPercentage: number;
+    // a css-property compatible color string of the ring i.e. "#FF0000" or "rgb(255, 0, 0)"
     color: string;
 }
