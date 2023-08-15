e4ed4920f5d619324180573b89a71dc9b68bf87c
JonasDoesThings <jonas@jonasdoesthings.com>
2023-08-15 09:46:44 +0000

    Deploying to gh-pages from @ JonasDoesThings/react-activity-rings@71b9d59e2642df796dc7da456ef4785b00bc7a2a 🚀

---
+++ assets/preview-0dc604a3.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-975864cd.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-bc124daa.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-bc124daa.js","./iframe-975864cd.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./_getPrototype-74a4a53f.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-0dc604a3.js.map
---
+++ assets/preview-37d8f0cb.js
@@ -0,0 +1,2 @@
+const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},layout:"centered",controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},backgrounds:{default:"black",values:[{name:"black",value:"#000"},{name:"white",value:"#FFF"},{name:"pink",value:"#F1337F"}]}}};export{e as default};
+//# sourceMappingURL=preview-37d8f0cb.js.map
